7199f368f1779a5e8a9c7b7c53774126aaddb7b0
patrick rock <prock@pretina.local>
2017-10-22 10:40:26 -0500

    solved 26

--- p26.hs
+++ p26.hs
@@ -1,6 +1,7 @@
 import Data.Maybe
 import Data.List 
 import Control.Applicative
+import Data.Function
 
 type Numerator = Int
 type Denominator = Int
@@ -14,28 +15,30 @@ data Decimal = Decimal Base Cycle
 --division (Fraction n d) 
 --  | mod n d == 0 = Decimal (Just (div n d)) Nothing
 
-division :: Int -> Int -> ([Int], Bool)
-division n d 
-  | mod n d == 0 = ([div n d], False)
-  | mod n d == 1 && n /= 1 = ([d], True)
-  | mod n d == div n 10 = ([d], True)
-  | otherwise = ((div n d) : (fst rec), snd rec)
-  where rec = (division ((mod n d) * 10 ) d) 
 
 remain n d = (mod n d) * 10
 --division' :: Int -> Int -> [Int]
-division' n d 
+division n d 
   | mod n d == 0 = [(div n d, (n, d))]
-  | otherwise = ((div n d), (n,d)) : (division' (remain n d) d)
+  | otherwise = ((div n d), (n,d)) : (division (remain n d) d)
 
 -- finds cycles of length n 
-findCycle xs n = findCycle' xs 0 n
+findCycle n xs = findCycle' xs 0 n
 
 findCycle' [] _ n = Nothing
 findCycle' (x:xs) p n
   | i /= Nothing = liftA2 (,) (Just p) (fmap (+(1+p)) i)
-  | otherwise = findCycle' xs (p+1)
+  | otherwise = findCycle' xs (p+1) n
   where i = elemIndex x $ take n xs
+
+cycleLength :: Maybe (Int, Int) -> Int
+cycleLength Nothing = 0 
+cycleLength (Just (a,b)) = b-a
+
+cycles = map (findCycle 1000) $ map (division 1) [1..]
+lengths = map cycleLength $ take 1000 cycles
+
+main = do print $ sortBy (compare `on` snd) $ zip [1..] lengths 
 
 -- division algorithm has two steps 
 -- 1. given a/b compute div a b and append to c
