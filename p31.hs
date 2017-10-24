1da500bfe4fe169e6052a741da14efcfed6ef727
patrick rock <prock@pretina.local>
2017-10-24 13:19:26 -0500

    finished 31

--- p31.hs
+++ p31.hs
@@ -1,23 +1,8 @@
-coinSum (a,b,c,d,e,f,g) = a + 2*b + 5*c + 10*d + 20*e + 50*f + 100*g == 200
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-candidates = [(a,b,c,d,e,g,f) | a <- [0..200],
-                                b <- [0..100],
-                                c <- [0..40],
-                                d <- [0..20],
-                                e <- [0..10],
-                                f <- [0..4],
-                                g <- [0..2]]
+changeN 0 x = 1
+changeN n x  = sum $ map (changeN (n-1))  [x-a*coin | a <- [0..lim] ]
+  where coin = coins !! n 
+        lim = div x coin
 
-p31 = filter coinSum candidates
-
-changex :: Int -> Int -> [[Int]] 
-changex x n 
-  | n-x < 0 = [[]]
-  | n-x == 0 = [[n]]
-  | otherwise = map  (++ [x]) (change (n-x))
-
-coins = [1,2,5,10,20,50,100,200] 
-
-change :: Int -> [[Int]]
-change n = concatMap ($n) changefs
-  where changefs = map changex $ filter (>=n) coins
+change = changeN 7
