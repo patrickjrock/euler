c2e33177e4a2c6b9dac8fd73f302119d88f211cc
patrick rock <prock@pretina.local>
2017-10-26 08:44:30 -0500

    39 done

--- p39.hs
+++ p39.hs
@@ -1,15 +1,20 @@
 import Data.List
+import Data.Ord
 
 euclid (m,n,k) =  (abs $ (m*m - n*n)*k, 2*m*n*k, (m*m+n*n)*k)
 
-triples l = map euclid [(m,n,k) | m <- [1..l],
-                                                    n <- [1..l],
-                                                    k <- [1..5],
-                                                    m /= n]
+triples = map euclid [(m,n,k) | m <- [1..23],
+                                  n <- [1..499],
+                                  k <- [1..100],
+                                  m /= n]
 
 
 eqTriples (a,b,c) (d,e,f) = (a==e && b==d) || (a==d && b==e)
 
 perimiter p (a,b,c) = a+b+c == p
 
-rightTriangles n p = nubBy eqTriples $ filter (perimiter p) (triples n)
+rightTriangles p = nubBy eqTriples $ filter (perimiter p) triples
+candidates = zip [1..1000] $ length <$> rightTriangles <$> [1..1000]
+
+
+main = do print $ sortBy (comparing snd) $ candidates  
