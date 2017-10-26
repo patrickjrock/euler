fc1d9325134521f4d6184e412cde4fc9b9e295fd
patrick rock <prock@pretina.local>
2017-10-26 10:14:40 -0500

    44 done

--- p44.hs
+++ p44.hs
@@ -1,14 +1,14 @@
 import Data.List
 
+isInt x = x == fromInteger (round x)
+
 ps =  toInteger <$> (\n->div (n*(3*n-1)) 2) <$> [1..] 
 pentagonal n = ps !! (n-1)
 pentagonals = map pentagonal [1..]
 
-ispent n = isp n pentagonals
-  where isp n (p:ps) 
-          | n == p = True
-          | n < p = False
-          | otherwise = isp n ps -- n < p
+isPent n = isInt $ inv $ fromInteger n
+  where inv x = (1/6)*(1 + sqrt (24*x + 1))
+
 
 
 pairs :: [a] -> [(a, a)]
@@ -16,11 +16,11 @@ pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
 
 cs n = pairs [1..n]
 
-candidate1 (a,b) = ispent (pa+pb)
+candidate1 (a,b) = isPent (pa+pb)
   where pa = pentagonal a
         pb = pentagonal b 
 
-candidate2 (a,b) = ispent $ abs (pa-pb)
+candidate2 (a,b) = isPent $ abs (pa-pb)
   where pa = pentagonal a
         pb = pentagonal b 
 
